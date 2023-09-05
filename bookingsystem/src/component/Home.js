28c96acfecc4b52cb98e5a1d354ccc450e18edc6
fmunir321 <fmunir321@gmail.com>
2023-09-05 23:33:28 +0500

    add date range on filter

--- bookingsystem/src/component/Home.js
+++ bookingsystem/src/component/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './Home.css';
 import { useState } from 'react';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
@@ -6,38 +6,72 @@ import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined
 import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { tourData } from './data.js';
+import { addDays } from 'date-fns';
+import { DateRange } from 'react-date-range';
+import moment from "moment";
+
 
 export default function Home() {
-    const [Location, setLocation] = useState({});
-    const [search, setSearch] = useState({});
-
-    const [isopen, setIsopen] = useState(false);
+    const [Search, setSearch] = useState({});
     const [filteredData, setFilteredData] = useState([])
+    const [dateshow, setDateshow] = useState(false);
+    const [state, setState] = useState([
+        {
+            startDate: new Date(),
+            endDate: addDays(new Date(), 7),
+            key: 'selection'
+        }
+    ]);
+    const refOne = useRef(null)
 
+    useEffect(() => {
+        document.addEventListener('keypress', hideOnEscape, true)
+        document.addEventListener('click', hideOnClickOutside, true)
+
+    }, [])
+
+    const hideOnEscape = (e) => {
+        if (e.key === "Escape") {
+            setDateshow(false);
+        }
+
+    }
+
+    const hideOnClickOutside = (e) => {
+        if (refOne.current && !refOne.current.contains(e.target)) {
+            setDateshow(false);
+        }
+    }
 
     function handlechange(event) {
-        setSearch({
-            ...search, [event.target.name]: event.target.value
-        })
+        setSearch({ ...Search, [event.target.name]: event.target.value });
 
         //For search 
-        const filterData = tourData.filter((el) => {
-            setIsopen(true)
-            //if no input the return the original
-            if (search.location === '') {
-                setFilteredData({ el })
-                return el;
-            }
-            //return the item which contains the user input
-            else {
-                console.log(search)
-                return el.name.toLowerCase().includes(event.target.value)
+        // const filterData = tourData.filter((el) => {
+        //     setIsopen(true)
+        //     //if no input the return the original
+        //     if (Search.location === '') {
+        //         setFilteredData({ el })
+        //         return el;
+        //     }
+        //     //return the item which contains the user input
+        //     else {
+        //         console.log("s")
+        //         return el.name.toLowerCase().includes(event.target.value)
 
-            }
+        //     }
 
-        })
-        setFilteredData(filterData);
+        // })
+        // setFilteredData(filterData);
 
+    }
+    const [search, setsearch] = useState('');
+    function submitrequest() {
+        const dateformate = {
+            startDate: moment(state[0].startDate).format('DD-MM-YYYY'),
+            endDate: moment(state[0].endDate).format('DD-MM-YYYY')
+        }
+        console.log(dateformate);
     }
 
     return (
@@ -53,41 +87,60 @@ export default function Home() {
             </div>
 
             <table>
-                <tr>
-                    <th>
-                        <label style={{ display: 'flex' }}> <LocationOnOutlinedIcon />Location</label>
-                        <input name='Location' onChange={handlechange} placeholder='Where you want you go?'></input>
-                    </th>
-                    <th>
-                        <label style={{ display: 'flex' }}> <CalendarMonthOutlinedIcon /> Choose Date</label>
-                        <input name='chooseDate' onChange={handlechange} placeholder='Choose Here'></input>
-                    </th>
-                    <th>
-                        <label style={{ display: 'flex' }}> <MonetizationOnOutlinedIcon />Price Range</label>
-                        {/* <input name='PriceRange' onChange={handlechange} placeholder='Choose Here'></input> */}
-                        <select name='chooseDate' style={{ border: 'none' }} onChange={handlechange}>
-                            <option value="">Choose Here</option>
-                            <option value="$50-$200">$50 - $200</option>
-                            <option value="$200-$400">$200 - $400</option>
-                            <option value="$400-$500">$400 - $500</option>
-                            <option value="$500-$750">$500 - $750</option>
-                            <option value="$750-$1000">$750 - $1000</option>
-                            <option value="$1000Above">$1000 Above</option>
-                        </select>
-                    </th>
-                    <th style={{ borderRadius: '20px', background: '#f35531', color: 'white' }}><SearchOutlinedIcon /></th>
-                </tr>
+
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <th>
+                            <label style={{ display: 'flex' }}> <LocationOnOutlinedIcon />Location</label>
+                            <input name='Location' onChange={(e) => setsearch(e.target.value)} placeholder='Where you want you go?'></input>
+                        </th>
+                        <th ref={refOne}>
+                            <label style={{ display: 'flex' }}> <CalendarMonthOutlinedIcon /> Choose Date</label>
+                            <input name='chooseDate' onChange={handlechange} onClick={() => setDateshow(true)} placeholder='Choose Here'></input>
+                        </th>
+                        <th>
+                            <label style={{ display: 'flex' }}> <MonetizationOnOutlinedIcon />Price Range</label>
+                            {/* <input name='PriceRange' onChange={handlechange} placeholder='Choose Here'></input> */}
+                            <select name='chooseDate' style={{ border: 'none' }} onChange={handlechange}>
+                                <option value="">Choose Here</option>
+                                <option value="$50-$200">$50 - $200</option>
+                                <option value="$200-$400">$200 - $400</option>
+                                <option value="$400-$500">$400 - $500</option>
+                                <option value="$500-$750">$500 - $750</option>
+                                <option value="$750-$1000">$750 - $1000</option>
+                                <option value="$1000Above">$1000 Above</option>
+                            </select>
+                        </th>
+                        <th style={{ borderRadius: '20px', background: '#f35531', color: 'white' }} onClick={submitrequest}><SearchOutlinedIcon /></th>
+                    </tr>
+                </tbody>
+                <tfoot></tfoot>
             </table >
 
-            {isopen ? <ul>
-                {filteredData.map((item) => (
-                    <lable className='lable'>{item.name}</lable>
-                ))}
-            </ul> : <ul>
-                {tourData.map((item) => (
-                    <lable className='lable'>{item.name}</lable>
-                ))}
-            </ul>}
+            {dateshow &&
+                <div style={{marginLeft:'50%'}}>
+                    <DateRange
+
+                        editableDateInputs={true}
+                        onChange={item => setState([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={state}
+                    />
+                </div>
+            }
+
+            <ul>
+                {tourData
+                    .filter((item) => {
+                        return search.toLowerCase() === ''
+                            ? item
+                            : item.name.toLowerCase().includes(search)
+                    })
+                    .map((item) => (
+                        <label className='lable'>{item.name}</label>
+                    ))}
+            </ul>
         </div >
     )
 }
--- bookingsystem/src/component/Test.js
+++ bookingsystem/src/component/Test.js
@@ -1,59 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { useLocation, useNavigate } from "react-router-dom";
 import moment from "moment";
 import { tourData } from "./data";
 import { useForm } from "react-hook-form";
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { addDays } from 'date-fns';
+import { DateRange } from 'react-date-range';
+import Moment from 'moment';
+import { Search } from "@mui/icons-material";
+
+
 
 function Test() {
-    const personalData = [
-        { firstname: 'Faisal', email: 'abc' }
-    ]
-    const [updateState, setUpdateState] = useState([]);
 
-    function pushData(data) {
-        setUpdateState(personalData[0]);
+    // code for date
+    const [dateshow, setDateshow] = useState(false);
+    const [state, setState] = useState([
+        {
+            startDate: new Date(),
+            endDate: addDays(new Date(), 7),
+            key: 'selection'
+        }
+    ]);
+
+    const refOne = useRef(null)
+
+    useEffect(() => {
+        document.addEventListener('keypress', hideOnEscape, true)
+        document.addEventListener('click', hideOnClickOutside, true)
+
+    }, [])
+
+    const hideOnEscape = (e) => {
+        if (e.key === "Escape") {
+            setDateshow(false);
+        }
+
     }
-    function handleInput(e) {
-        const { name, value } = e.target;
 
-        setUpdateState({
-            ...updateState,
-            [name]: value
-        });
+    const hideOnClickOutside = (e) => {
+        if (refOne.current && !refOne.current.contains(e.target)) {
+            setDateshow(false);
+        }
     }
 
-    function savetData() {
-        console.log(updateState)
+    function consolelog() {
+
+        const dateformate = {
+            startDate: moment(state[0].startDate).format('DD-MM-YYYY'),
+            endDate: moment(state[0].endDate).format('DD-MM-YYYY')
+        }
     }
 
-    function updateData() {
-        console.log('click update button');
-        console.log(updateState)
+    function handlechange() {
+        setDateshow(true)
     }
-
     return (
-        <div>test page
-            {
-                personalData.map((item) => (
-                    <div>
-                        <p>{item.firstname}</p><br />
-                        <p>{item.email}</p>
-                        <button onClick={() => pushData(item)}>Edit</button>
-                    </div>
-                ))
-            }
 
-            <form>
-                <label>Name:</label><br />
-                <input type='text' id='email' name='firstname' value={updateState.firstname} onChange={handleInput} ></input><br />
+        // code for Date
+        <div>
+            <div ref={refOne}>
+                <input name='chooseDate' onClick={handlechange} placeholder='Choose Here'></input>
 
-                <label>Email</label><br />
-                <input type='text' id='email' name='email' value={updateState.email} onChange={handleInput} ></input><br />
+                {dateshow &&
+                    <DateRange
+                        editableDateInputs={true}
+                        onChange={item => setState([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={state}
+                    />
+                }
 
-            </form>
-            <button onClick={updateData}>Save</button>
+                <button style={{ margin: '6%' }} onClick={consolelog}>date range</button>
+            </div>
         </div>
     );
 };
